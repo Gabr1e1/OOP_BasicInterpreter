940ab98f7565b094feb77bccd9f199c983339fad
Gabriel <zhangzhicheng1@126.com>
2018-11-30 13:15:33 +0800

    Update Basic.cpp

--- Basic.cpp
+++ Basic.cpp
@@ -67,7 +67,7 @@ int main()
 
 ResultType processLine(string line, Program & program, EvalState & state)
 {
-	while (line[line.length() - 1] == " ") line = line.substr(0, line.length() - 2);
+	while (line[line.length() - 1] == ' ') line = line.substr(0, line.length() - 2);
 	if (line.length() == 0) return EXECUTED;
 	StatementType curType = analyzeStatement(line);
 	if (curType == COMMAND)
